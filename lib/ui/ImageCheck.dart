c740219905e0142d0a06e919a5dbb3a5e4975ff6
JC-210611 <210611@jc-21.jp>
2024-01-09 13:06:07 +0900

    照合結果を受けてそれぞれの画面に遷移するように修正した

--- lib/ui/ImageCheck.dart
+++ lib/ui/ImageCheck.dart
@@ -23,8 +23,6 @@ class _ImageCheckState extends State<ImageCheck> {
   Image? imagepath;
 
   Widget build(BuildContext context) {
-    print("Build method is called.");
-
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: AppBar(
