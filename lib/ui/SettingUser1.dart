113cbbd5e9bceaf8579dfbcbf03230439acb917f
JC21-210277 <210277@jc-21.jp>
2024-01-23 10:58:27 +0900

    背景色の追加

--- lib/ui/SettingUser1.dart
+++ lib/ui/SettingUser1.dart
@@ -25,97 +25,124 @@ class UserSettings1 extends State<StateUserSettings1>{
 
   @override
   Widget build(BuildContext context){
-    return Scaffold(
-      appBar:AppbarComp(),
-      body: Center(
-          child:SingleChildScrollView(
-            child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:[
-                  Container(
-                    margin:const EdgeInsets.fromLTRB(0, 10, 0, 10),
-                    padding:const EdgeInsets.fromLTRB(40, 10, 40, 10),
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(10),
-                      border: Border.all(
-                        color: Colors.deepOrange,
-                        width: 1,
-                      ),
-                    ),
-                    child:const Text('ユーザー設定',
-                      style: TextStyle(
-                          fontSize: 30,
-                          color:Colors.deepOrange,
-                          fontWeight: FontWeight.bold
-                      ),
-                    ),
-                  ),
-                  Container(
-                    margin:const EdgeInsets.all(20),
-                    child: const FittedBox(
-                      child: Text('登録内容を変更する\nユーザーを選択してください',
-                        style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        border: Border.all(
-                            color:Colors.deepOrange)
-                    ),
-                    child:Container(
-                      width:265,
-                      padding: const EdgeInsets.all(15),
-                      margin: const EdgeInsets.all(3),
-                      decoration: BoxDecoration(
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors:[Colors.white,Color(0xFFFAAC90)],
+        )
+      ),
+      child:Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar:AppbarComp(),
+        body: Center(
+          child:Container(
+              width: 300,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+                boxShadow: const [
+                  BoxShadow(
+                      color:Colors.black12,
+                      blurRadius: 2,
+                      spreadRadius: 2,
+                      offset: Offset(7,7)
+                  )
+                ],
+              ),
+              child:SingleChildScrollView(
+                child:Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      Container(
+                        margin:const EdgeInsets.fromLTRB(0, 30, 0, 10),
+                        padding:const EdgeInsets.fromLTRB(40, 10, 40, 10),
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(10),
                           border: Border.all(
-                              color:Colors.deepOrange)
+                            color: Colors.deepOrange,
+                            width: 1,
+                          ),
+                        ),
+                        child:const Text('ユーザー設定',
+                          style: TextStyle(
+                              fontSize: 30,
+                              color:Colors.deepOrange,
+                              fontWeight: FontWeight.bold
+                          ),
+                        ),
                       ),
-                      child:Column(
-                        children: [
+                      Container(
+                        margin:const EdgeInsets.all(15),
+                        child: const FittedBox(
+                          child: Text('登録内容を変更する\nユーザーを選択してください',
+                            style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+                      Container(
+                        margin: EdgeInsets.fromLTRB(0, 10, 0, 40),
+                        decoration: BoxDecoration(
+                            border: Border.all(
+                                color:Colors.deepOrange)
+                        ),
+                        child:Container(
+                          width:265,
+                          padding: const EdgeInsets.all(15),
+                          margin: const EdgeInsets.all(3),
+                          decoration: BoxDecoration(
+                              border: Border.all(
+                                  color:Colors.deepOrange)
+                          ),
+                          child:Column(
+                            children: [
 
-                          if(DBuser.userName.isEmpty)...[
-                            Container(
-                              margin:const EdgeInsets.fromLTRB(0, 20, 0, 20),
-                              child:const FittedBox(
-                                child:Text('ユーザーが\n登録されていません',
-                                  style:TextStyle(
-                                      fontSize: 25,color:Colors.black87,
-                                      fontWeight: FontWeight.bold,
-                                  ),textAlign: TextAlign.center,
+                              if(DBuser.userName.isEmpty)...[
+                                Container(
+                                  margin:const EdgeInsets.fromLTRB(0, 20, 0, 20),
+                                  child:const FittedBox(
+                                    child:Text('ユーザーが\n登録されていません',
+                                      style:TextStyle(
+                                        fontSize: 25,color:Colors.black87,
+                                        fontWeight: FontWeight.bold,
+                                      ),textAlign: TextAlign.center,
+                                    ),
+                                  ),
                                 ),
-                              ),
-                            ),
-                          ],
-                          for(String n in DBuser.userName)...[
-                            Container(
-                              width: 240,
-                              height: 50,
-                              margin: const EdgeInsets.all(2),
-                              child:OutlinedButton(
-                                style:OutlinedButton.styleFrom(
-                                    side:const BorderSide(
-                                        color: Colors.deepOrangeAccent
-                                    )
+                              ],
+                              for(String n in DBuser.userName)...[
+                                Container(
+                                  width: 240,
+                                  height: 50,
+                                  margin: const EdgeInsets.all(2),
+                                  child:OutlinedButton(
+                                    style:OutlinedButton.styleFrom(
+                                        side:const BorderSide(
+                                            color: Colors.deepOrangeAccent
+                                        )
+                                    ),
+                                    child:Text(n,style:const TextStyle(fontSize: 25,color:Colors.black87)),
+                                    onPressed: (){
+                                      PageRoute(n);
+                                    },
+                                  ),
                                 ),
-                                child:Text(n,style:const TextStyle(fontSize: 25,color:Colors.black87)),
-                                onPressed: (){
-                                  PageRoute(n);
-                                },
-                              ),
-                            ),
-                          ]
+                              ]
 
-                        ],
+                            ],
+                          ),
+
+                        ),
                       ),
 
-                    ),
-                  ),
+                    ]
+                ),
+              )
+          ),
 
-                ]
-            ),
-          )
+        ),
       ),
     );
   }
