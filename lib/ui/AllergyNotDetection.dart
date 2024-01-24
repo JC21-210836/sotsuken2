7705897547448345efe3554b2fcac1b0bac8e3b3
JC-21\210277 <210277@jc-21.jp>
2024-01-24 16:22:01 +0900

    読み込み結果の画面全体的に変更

--- lib/ui/AllergyNotDetection.dart
+++ lib/ui/AllergyNotDetection.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 
 import '../component/AppbarComp.dart';
 import '../ui/ReadIngredient.dart';
-
-import 'ImageLoaderSelect.dart';
+import '../ui/ImageLoaderSelect.dart';
+import '../component/BottomNavbarComp.dart';
 
 class StateAllergyNotDetection extends StatefulWidget{
   const StateAllergyNotDetection({super.key});
@@ -29,165 +29,230 @@ class AllergyNotDetection_Page extends State<StateAllergyNotDetection>{
       child:Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppbarComp(),
+        bottomNavigationBar: const BottomNavbarCompState(flagName: 'none', text: '未定',),
         body: Center(
-          child:Container(
-              width: 300,
-              decoration: BoxDecoration(
-                borderRadius: BorderRadius.circular(20),
-                color: Colors.white,
-                boxShadow: const [
-                  BoxShadow(
-                      color:Colors.black12,
-                      blurRadius: 2,
-                      spreadRadius: 2,
-                      offset: Offset(7,7)
-                  )
-                ],
+          child:Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Container(
+                width: 300,
+                margin: EdgeInsets.fromLTRB(0, 0, 0, 15),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(20),
+                  color: Colors.white,
+                  boxShadow: const [
+                    BoxShadow(
+                        color:Colors.black12,
+                        blurRadius: 2,
+                        spreadRadius: 2,
+                        offset: Offset(4,4)
+                    )
+                  ],
+                ),
+                child:Container(
+                  alignment: Alignment.center,
+                  margin: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                  padding: const EdgeInsets.fromLTRB(0, 0, 0, 5),
+                  child:  Container(
+                    alignment: Alignment.center,
+                    width: 200,
+                    padding: const EdgeInsets.fromLTRB(0, 12, 0, 7),
+                    decoration: const BoxDecoration(
+                      border:Border(
+                          bottom: BorderSide(
+                              color: Colors.blue
+                          )
+                      ),
+                    ),
+
+                    child:RichText(
+                      text: const TextSpan(
+                          children: [
+                            TextSpan(
+                              text:'読み込み結果',
+                              style: TextStyle(
+                                fontSize: 28,
+                                color:Colors.indigo,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ]
+                      ),
+                    ),
+
+                  ),
+                ),
               ),
-            child:SingleChildScrollView(
-              child:Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children:<Widget>[
-
-                  Container(
-                    margin: EdgeInsets.fromLTRB(9, 30, 9, 0),
-                    decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        border: Border.all(color:Colors.indigo)
-                    ),
-                    child: Container(
-                      margin: const EdgeInsets.all(2),
-                      padding: const EdgeInsets.fromLTRB(40, 10, 40, 10),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(5),
-                          border: Border.all(color:Colors.indigo)
-                      ),
-                      child:const Text('読み込み結果',
-                        style: TextStyle(
-                            fontSize: 26,
-                            color:Colors.indigo,
-                            fontWeight: FontWeight.bold
-                        ),
-                      ),
-                    ),
+              Container(
+                  width: 300,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.white,
+                    boxShadow: const [
+                      BoxShadow(
+                          color:Colors.black12,
+                          blurRadius: 2,
+                          spreadRadius: 2,
+                          offset: Offset(7,7)
+                      )
+                    ],
                   ),
-                  Container(
-                    margin: const EdgeInsets.fromLTRB(20, 30, 20, 0),
-                    child:const FittedBox(
-                      child:Text('指定されたアレルゲンは\n見つかりませんでした。',
-                        style: TextStyle(
-                            color:Colors.indigo,
-                            fontSize: 22,
-                            fontWeight: FontWeight.bold
-                        ),
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                  ),
-
-                  Container(
-                    width: 280,
-                    margin:const EdgeInsets.fromLTRB(0, 0, 0, 0),
-                    child:Image.asset('images/duck.png'),
-                  ),
-
-                  Container(
-                    height: 55,
-                    width: 260,
-                    margin: const EdgeInsets.all(10),
-                    child:ElevatedButton(
-                      style:ElevatedButton.styleFrom(
-                          backgroundColor: Colors.orange[700],
-                          shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15)
-                          ),
-                          elevation: 7
-                      ),
-                      onPressed: (){
-                        Navigator.of(context).push(
-                            MaterialPageRoute(builder: (context){
-                              return const StateImageLoderSelect();
-                            })
-                        );
-                      },
-                      child: const FittedBox(
-                        child: Text('他の商品をスキャンする',
-                          style:TextStyle(
-                            fontSize: 24,
-                            color:Colors.white,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Container(
-                          margin: const EdgeInsets.fromLTRB(0,0,5,30),
-                          height: 70,
-                          width: 140,
-                          child:ElevatedButton(
-                            style:ElevatedButton.styleFrom(
-                                backgroundColor: Colors.blue,
-                                shape: RoundedRectangleBorder(
-                                    borderRadius: BorderRadius.circular(15)
-                                ),
-                                elevation: 7
+                child:SingleChildScrollView(
+                  child:Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:<Widget>[
+                      Container(
+                        margin: const EdgeInsets.fromLTRB(20, 40, 20, 0),
+                        child:const FittedBox(
+                          child:Text('指定されたアレルゲンは\n見つかりませんでした。',
+                            style: TextStyle(
+                                color:Colors.indigo,
+                                fontSize: 22,
+                                fontWeight: FontWeight.bold
                             ),
-                            onPressed: (){
-                              Navigator.popUntil(context,ModalRoute.withName('ChooseUser_page'));
-                            },
-                            child: const FittedBox(
-                              child:Text('他のユーザー\nを選択する',
-                                  style:TextStyle(
-                                    fontSize: 20,
-                                    color:Colors.white,
-                                    fontWeight: FontWeight.bold,
-                                  ),
-                                  textAlign:TextAlign.center
+                            textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
+
+                      Container(
+                        width: 280,
+                        margin:const EdgeInsets.fromLTRB(0, 0, 0, 0),
+                        child:Image.asset('images/duck.png'),
+                      ),
+
+                      Container(
+                        height: 55,
+                        width: 260,
+                        margin: const EdgeInsets.all(10),
+                        child:ElevatedButton(
+                          style:ElevatedButton.styleFrom(
+                              backgroundColor: Colors.orange[700],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)
+                              ),
+                              elevation: 7
+                          ),
+                          onPressed: (){
+                            Navigator.of(context).push(
+                                MaterialPageRoute(builder: (context){
+                                  return const StateImageLoderSelect();
+                                })
+                            );
+                          },
+                          child: const FittedBox(
+                            child: Text('他の商品をスキャンする',
+                              style:TextStyle(
+                                fontSize: 24,
+                                color:Colors.white,
+                                fontWeight: FontWeight.bold,
                               ),
                             ),
                           ),
                         ),
-                        Container(
-                          margin: const EdgeInsets.fromLTRB(5,0,0,30),
-                          padding:EdgeInsets.fromLTRB(3, 0, 3, 0),
-                          height: 70,
-                          width: 130,
-                          child:ElevatedButton(
-                              style:ElevatedButton.styleFrom(
-                                  backgroundColor: Colors.blue,
-                                  shape: RoundedRectangleBorder(
-                                      borderRadius: BorderRadius.circular(15)
-                                  ),
-                                  elevation: 7
+                      ),
+                      Container(
+                        height: 55,
+                        width: 260,
+                        margin: const EdgeInsets.fromLTRB(10,10,10,30),
+                        child:ElevatedButton(
+                          style:ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue[700],
+                              shape: RoundedRectangleBorder(
+                                  borderRadius: BorderRadius.circular(15)
                               ),
-                              child: const FittedBox(
-                                child:Text('読み取った\n成分を見る',
-                                    style:TextStyle(
-                                      fontSize: 20,
-                                      color:Colors.white,
-                                      fontWeight: FontWeight.bold,
+                              elevation: 7
+                          ),
+                          onPressed: (){
+                            Navigator.of(context).push(
+                                MaterialPageRoute(builder: (context){
+                                  return ReadIngredient();
+                                })
+                            );
+                          },
+                          child: const FittedBox(
+                            child: Text('読み取った成分を見る',
+                              style:TextStyle(
+                                fontSize: 24,
+                                color:Colors.white,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                      /*
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(0,0,5,30),
+                              height: 70,
+                              width: 140,
+                              child:ElevatedButton(
+                                style:ElevatedButton.styleFrom(
+                                    backgroundColor: Colors.blue,
+                                    shape: RoundedRectangleBorder(
+                                        borderRadius: BorderRadius.circular(15)
                                     ),
-                                    textAlign:TextAlign.center
+                                    elevation: 7
+                                ),
+                                onPressed: (){
+                                  Navigator.popUntil(context,ModalRoute.withName('ChooseUser_page'));
+                                },
+                                child: const FittedBox(
+                                  child:Text('他のユーザー\nを選択する',
+                                      style:TextStyle(
+                                        fontSize: 20,
+                                        color:Colors.white,
+                                        fontWeight: FontWeight.bold,
+                                      ),
+                                      textAlign:TextAlign.center
+                                  ),
                                 ),
                               ),
-                              onPressed: (){
-                                Navigator.of(context).push(
-                                    MaterialPageRoute(builder: (context){
-                                      return ReadIngredient();
-                                    })
-                                );
-                              }
-                          ),
-                        )
-                      ]
-                  )
-                ],
+                            ),
+                            Container(
+                              margin: const EdgeInsets.fromLTRB(5,0,0,30),
+                              padding:EdgeInsets.fromLTRB(3, 0, 3, 0),
+                              height: 70,
+                              width: 130,
+                              child:ElevatedButton(
+                                  style:ElevatedButton.styleFrom(
+                                      backgroundColor: Colors.blue,
+                                      shape: RoundedRectangleBorder(
+                                          borderRadius: BorderRadius.circular(15)
+                                      ),
+                                      elevation: 7
+                                  ),
+                                  child: const FittedBox(
+                                    child:Text('読み取った\n成分を見る',
+                                        style:TextStyle(
+                                          fontSize: 20,
+                                          color:Colors.white,
+                                          fontWeight: FontWeight.bold,
+                                        ),
+                                        textAlign:TextAlign.center
+                                    ),
+                                  ),
+                                  onPressed: (){
+                                    Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context){
+                                          return ReadIngredient();
+                                        })
+                                    );
+                                  }
+                              ),
+                            )
+                          ]
+                      )
+
+                       */
+                    ],
+                  ),
+                )
               ),
-            )
+            ],
           ),
         ),
       ),
