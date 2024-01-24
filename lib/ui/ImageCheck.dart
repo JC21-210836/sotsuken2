6e15bd541816815e7974c80b2c1d0ee5a506cc34
JC-21\210277 <210277@jc-21.jp>
2024-01-24 13:05:54 +0900

    なのはちゃんの延期してた部分(背景色とか)

--- lib/ui/ImageCheck.dart
+++ lib/ui/ImageCheck.dart
@@ -25,202 +25,219 @@ class _ImageCheckState extends State<ImageCheck> {
   Image? imagepath;
 
   Widget build(BuildContext context) {
-    return Scaffold(
-      resizeToAvoidBottomInset: false,
-      appBar: AppbarComp(),
-      body: Center(
-        child: Stack(
-          alignment: Alignment.center,
-          children: [
-            SingleChildScrollView(
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors:[Colors.white,Color(0xFF90D4FA)],
+        )
+      ),
+      child:Scaffold(
+        backgroundColor: Colors.transparent,
+        resizeToAvoidBottomInset: false,
+        appBar: AppbarComp(),
+        body: Center(
+          child:Column(
+            children: [
+
+
+              //↓これ消しちゃダメ
+              Stack(
+                alignment: Alignment.center,
                 children: [
-                  Container(
-                    padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
-                    margin: const EdgeInsets.fromLTRB(10, 30, 10, 20),
-                    decoration: BoxDecoration(
-                      border: Border.all(color: Colors.blue),
-                    ),
-                    child: Container(
-                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                      decoration: BoxDecoration(
-                        border: Border.all(color: Colors.blue),
-                      ),
-                      child: const Text(
-                        '写真のスキャン',
-                        style: TextStyle(
-                            color: Colors.indigo,
-                            fontSize: 27,
-                            fontWeight: FontWeight.bold),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    width: 250,
-                    child:ClipRRect(
-                      borderRadius: BorderRadius.circular(5),
-                      child: imagepath ?? Image.file(File(widget.image.path)),
-                    )
-                  ),
-                  Container(
-                    padding: const EdgeInsets.all(8),
-                    child: const Text(
-                      'この画像でよろしいですか？',
-                      style: TextStyle(
-                          color: Colors.indigo,
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold),
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        width: 100,
-                        height: 50,
-                        margin: EdgeInsets.fromLTRB(7, 5, 20, 15),
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                            side: const BorderSide(
-                              color: Colors.deepOrange,
+                  SingleChildScrollView(
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                          margin: const EdgeInsets.fromLTRB(10, 30, 10, 20),
+                          decoration: BoxDecoration(
+                            border: Border.all(color: Colors.blue),
+                          ),
+                          child: Container(
+                            padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.blue),
+                            ),
+                            child: const Text(
+                              '写真のスキャン',
+                              style: TextStyle(
+                                  color: Colors.indigo,
+                                  fontSize: 27,
+                                  fontWeight: FontWeight.bold),
                             ),
                           ),
-                          child: const Text(
-                            'いいえ',
-                            style: TextStyle(
-                                color: Colors.deepOrange,
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold),
-                          ),
-                          onPressed: () async {
-                            Navigator.pop(context);
-                          },
                         ),
-                      ),
-                      Container(
-                        width: 100,
-                        height: 50,
-                        margin: EdgeInsets.fromLTRB(7, 5, 20, 15),
-                        child: OutlinedButton(
-                          style: OutlinedButton.styleFrom(
-                              side: const BorderSide(
-                                color: Colors.indigo,
-                              )),
+                        Container(
+                          width: 250,
+                          child:ClipRRect(
+                            borderRadius: BorderRadius.circular(5),
+                            child: imagepath ?? Image.file(File(widget.image.path)),
+                          )
+                        ),
+                        Container(
+                          padding: const EdgeInsets.all(8),
                           child: const Text(
-                            'はい',
+                            'この画像でよろしいですか？',
                             style: TextStyle(
                                 color: Colors.indigo,
                                 fontSize: 18,
                                 fontWeight: FontWeight.bold),
+                            textAlign: TextAlign.center,
                           ),
-                          onPressed: () async {
-                            try{
-                              setState(() {
-                                isLoading = true;
-                              });
-                              XFile xFileimage = cropimage ?? widget.image;
-                              await Api.instance.postData(xFileimage);
-                              List<String> content = await verifications.instance.verification();
-                              setState(() {
-                                isLoading = false;
-                              });
-                              if (!content.contains("No")) {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(builder: (context) {
-                                    return StateAllergyDetection();
-                                  }),
-                                );
-                              } else {
-                                Navigator.of(context).push(
-                                  MaterialPageRoute(builder: (context) {
-                                    return StateAllergyNotDetection();
-                                  }),
-                                );
-                              }
-                            }catch(error){
-                              setState(() {
-                                isLoading = false;
-                              });
-                              showDialog(context: context, builder: (context){
-                                return AlertDialog(
-                                  title: Row(
-                                    children: [
-                                      Icon(
-                                        Icons.error,
-                                        color: Colors.red,
-                                      ),
-                                      Text('ネットワークエラー'),
-                                    ],
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Container(
+                              width: 100,
+                              height: 50,
+                              margin: EdgeInsets.fromLTRB(7, 5, 20, 15),
+                              child: OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                  side: const BorderSide(
+                                    color: Colors.deepOrange,
                                   ),
-                                  content: const Text('接続状況を確認してください'),
-                                  actions: [
-                                    TextButton(onPressed: (){
-                                      Navigator.pop(context);
-                                    }, child: Text('OK')),
-                                  ],
-                                );
-                              });
-                            }
-                          },
+                                ),
+                                child: const Text(
+                                  'いいえ',
+                                  style: TextStyle(
+                                      color: Colors.deepOrange,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onPressed: () async {
+                                  Navigator.pop(context);
+                                },
+                              ),
+                            ),
+                            Container(
+                              width: 100,
+                              height: 50,
+                              margin: EdgeInsets.fromLTRB(7, 5, 20, 15),
+                              child: OutlinedButton(
+                                style: OutlinedButton.styleFrom(
+                                    side: const BorderSide(
+                                      color: Colors.indigo,
+                                    )),
+                                child: const Text(
+                                  'はい',
+                                  style: TextStyle(
+                                      color: Colors.indigo,
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                onPressed: () async {
+                                  try{
+                                    setState(() {
+                                      isLoading = true;
+                                    });
+                                    XFile xFileimage = cropimage ?? widget.image;
+                                    await Api.instance.postData(xFileimage);
+                                    List<String> content = await verifications.instance.verification();
+                                    setState(() {
+                                      isLoading = false;
+                                    });
+                                    if (!content.contains("No")) {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context) {
+                                          return StateAllergyDetection();
+                                        }),
+                                      );
+                                    } else {
+                                      Navigator.of(context).push(
+                                        MaterialPageRoute(builder: (context) {
+                                          return StateAllergyNotDetection();
+                                        }),
+                                      );
+                                    }
+                                  }catch(error){
+                                    setState(() {
+                                      isLoading = false;
+                                    });
+                                    showDialog(context: context, builder: (context){
+                                      return AlertDialog(
+                                        title: Row(
+                                          children: [
+                                            Icon(
+                                              Icons.error,
+                                              color: Colors.red,
+                                            ),
+                                            Text('ネットワークエラー'),
+                                          ],
+                                        ),
+                                        content: const Text('接続状況を確認してください'),
+                                        actions: [
+                                          TextButton(onPressed: (){
+                                            Navigator.pop(context);
+                                          }, child: Text('OK')),
+                                        ],
+                                      );
+                                    });
+                                  }
+                                },
+                              ),
+                            ),
+                          ],
                         ),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    child: ElevatedButton.icon(
-                      icon: const Icon(
-                        Icons.crop,
-                        color: Colors.white,
-                      ),
-                      label: Text(state),
-                      style: ElevatedButton.styleFrom(
-                        foregroundColor: Colors.white, backgroundColor: Colors.blue,
-                      ),
-                      onPressed: () async {
-                        print("ボタン押されたよ:$state");
-                        if (state == "トリミング") {
-                          await Crop.cropImage(widget.image, (croppedFile) {
-                            print("トリミングだった：$croppedFile");
-                            if (croppedFile != null) {
-                              setState(() {
-                                cropimage = XFile(croppedFile.path);
-                                imagepath = Image.file(File(cropimage!.path));
-                                state = "クリア";
-                                print("cropimage:$cropimage");
-                                print("imagepath:$imagepath");
-                                print("state:$state");
-                              });
-                            }
-                          });
-                        } else if (state == "クリア") {
-                          Crop.clearImage((clearedImage) {
-                            print("クリアだった：$clearedImage");
-                            if (clearedImage == null) {
-                              setState(() {
-                                cropimage = null;
-                                imagepath = null;
-                                state = "トリミング";
-                              });
-                            }
-                          });
-                        }
-                      },
+                        Container(
+                          child: ElevatedButton.icon(
+                            icon: const Icon(
+                              Icons.crop,
+                              color: Colors.white,
+                            ),
+                            label: Text(state),
+                            style: ElevatedButton.styleFrom(
+                              foregroundColor: Colors.white, backgroundColor: Colors.blue,
+                            ),
+                            onPressed: () async {
+                              print("ボタン押されたよ:$state");
+                              if (state == "トリミング") {
+                                await Crop.cropImage(widget.image, (croppedFile) {
+                                  print("トリミングだった：$croppedFile");
+                                  if (croppedFile != null) {
+                                    setState(() {
+                                      cropimage = XFile(croppedFile.path);
+                                      imagepath = Image.file(File(cropimage!.path));
+                                      state = "クリア";
+                                      print("cropimage:$cropimage");
+                                      print("imagepath:$imagepath");
+                                      print("state:$state");
+                                    });
+                                  }
+                                });
+                              } else if (state == "クリア") {
+                                Crop.clearImage((clearedImage) {
+                                  print("クリアだった：$clearedImage");
+                                  if (clearedImage == null) {
+                                    setState(() {
+                                      cropimage = null;
+                                      imagepath = null;
+                                      state = "トリミング";
+                                    });
+                                  }
+                                });
+                              }
+                            },
+                          ),
+                        ),
+                      ],
                     ),
                   ),
+                  // プログレスバーの表示
+                  if (isLoading)
+                    Container(
+                      color: Colors.black.withOpacity(0.5),
+                      child: const Center(
+                        child: CircularProgressIndicator(),
+                      ),
+                    ),
                 ],
               ),
-            ),
-            // プログレスバーの表示
-            if (isLoading)
-              Container(
-                color: Colors.black.withOpacity(0.5),
-                child: const Center(
-                  child: CircularProgressIndicator(),
-                ),
-              ),
-          ],
+            ],
+          )
         ),
       ),
     );
--- lib/ui/ImageLoaderSelect.dart
+++ lib/ui/ImageLoaderSelect.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'ImageCheck.dart';
+import '../component/AppbarComp.dart';
 
 class StateImageLoderSelect extends StatefulWidget{
   const StateImageLoderSelect({super.key});
@@ -16,114 +17,122 @@ class ImageLoderSelect extends State<StateImageLoderSelect> {
   @override
   Widget build(BuildContext context) {
     final ImagePicker _picker = ImagePicker();
-    return Scaffold(
-      appBar: AppBar(
-          toolbarHeight: 60,
-          title: const Text('成分チェッカー')
-      ),
-      body: Center(
-        child:Center(
-          child:SingleChildScrollView(
-            child:Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Container(
-                  padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
-                  margin: const EdgeInsets.fromLTRB(10, 30, 10, 40),
-                  decoration: BoxDecoration(
-                    border: Border.all(color:Colors.blue),
-                  ),
-                  child:Container(
-                    padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+    return Container(
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors:[Colors.white,Color(0xFF90D4FA)],
+          )
+        ),
+      child:Scaffold(
+        backgroundColor: Colors.transparent,
+        appBar: AppbarComp(),
+        body: Center(
+          child:Center(
+            child:SingleChildScrollView(
+              child:Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(5, 5, 5, 5),
+                    margin: const EdgeInsets.fromLTRB(10, 30, 10, 40),
                     decoration: BoxDecoration(
                       border: Border.all(color:Colors.blue),
                     ),
-                    child:const Text('写真のスキャン',
-                      style:TextStyle(
-                          color: Colors.indigo,
-                          fontSize: 27,
-                          fontWeight: FontWeight.bold
+                    child:Container(
+                      padding: const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                      decoration: BoxDecoration(
+                        border: Border.all(color:Colors.blue),
+                      ),
+                      child:const Text('写真のスキャン',
+                        style:TextStyle(
+                            color: Colors.indigo,
+                            fontSize: 27,
+                            fontWeight: FontWeight.bold
+                        ),
                       ),
                     ),
                   ),
-                ),
 
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 25, 10, 25),
-                  width: 230,
-                  height: 70,
-                  child:ElevatedButton(
-                    style:ElevatedButton.styleFrom(
-                        backgroundColor: Colors.indigo
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 25, 10, 25),
+                    width: 230,
+                    height: 70,
+                    child:ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                          backgroundColor: Colors.indigo
+                      ),
+                      child: const FittedBox(
+                        child:Text('デバイスの\nライブラリから取得',
+                          style:TextStyle(
+                              fontSize: 21,
+                              fontWeight: FontWeight.bold
+                          ),
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      onPressed: () async {
+                        var imageSource = ImageSource.gallery;
+                        final XFile? image = await _picker.pickImage(
+                            source: imageSource
+                        );
+                        if (image != null){
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => ImageCheck(image),),);
+                        }else{
+                          showDialog(context: context, builder: (context){
+                            return AlertDialog(
+                              title: Row(
+                                children: [
+                                  Icon(
+                                    Icons.error,
+                                    color: Colors.yellow,
+                                  ),
+                                  Text('エラー'),
+                                ],
+                              ),
+                              content: const Text('読み込みができませんでした。\n他の画像を選択してください'),
+                              actions: [
+                                TextButton(onPressed: (){
+                                  Navigator.pop(context);
+                                }, child: Text('OK')),
+                              ],
+                            );
+                          });
+                        }
+                      },
                     ),
-                    child: const FittedBox(
-                      child:Text('デバイスの\nライブラリから取得',
+                  ),
+                  Container(
+                    margin: EdgeInsets.fromLTRB(10, 25, 10, 25),
+                    width: 230,
+                    height: 70,
+                    child:ElevatedButton(
+                      style:ElevatedButton.styleFrom(
+                          backgroundColor: Colors.indigo
+                      ),
+                      child: const Text('カメラで撮影する',
                         style:TextStyle(
-                            fontSize: 21,
+                            fontSize: 20,
                             fontWeight: FontWeight.bold
                         ),
                         textAlign: TextAlign.center,
                       ),
+                      onPressed: () async {
+                        final XFile? image = await _picker.pickImage(
+                          source: ImageSource.camera,
+                        );
+                        if (image != null)
+                          Navigator.push(context, MaterialPageRoute(builder: (context) => ImageCheck(image),),);
+                      },
                     ),
-                    onPressed: () async {
-                      var imageSource = ImageSource.gallery;
-                      final XFile? image = await _picker.pickImage(
-                          source: imageSource
-                      );
-                      if (image != null){
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => ImageCheck(image),),);
-                      }else{
-                        showDialog(context: context, builder: (context){
-                          return AlertDialog(
-                            title: Row(
-                              children: [
-                                Icon(
-                                  Icons.error,
-                                  color: Colors.yellow,
-                                ),
-                                Text('エラー'),
-                              ],
-                            ),
-                            content: const Text('読み込みができませんでした。\n他の画像を選択してください'),
-                            actions: [
-                              TextButton(onPressed: (){
-                                Navigator.pop(context);
-                              }, child: Text('OK')),
-                            ],
-                          );
-                        });
-                      }
-                    },
                   ),
-                ),
-                Container(
-                  margin: EdgeInsets.fromLTRB(10, 25, 10, 25),
-                  width: 230,
-                  height: 70,
-                  child:ElevatedButton(
-                    style:ElevatedButton.styleFrom(
-                        backgroundColor: Colors.indigo
-                    ),
-                    child: const Text('カメラで撮影する',
-                      style:TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold
-                      ),
-                      textAlign: TextAlign.center,
-                    ),
-                    onPressed: () async {
-                      final XFile? image = await _picker.pickImage(
-                        source: ImageSource.camera,
-                      );
-                      if (image != null)
-                        Navigator.push(context, MaterialPageRoute(builder: (context) => ImageCheck(image),),);
-                    },
-                  ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
 
+          ),
         ),
       ),
     );
